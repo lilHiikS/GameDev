ac71bebe3898f83a8701a19fe39a7b94b7ae0c56
Oliver Bjørn Diangson Rosenkilde <olros.dev@proton.me>
2025-10-21 10:23:20 +0200

    start script

--- Shadowless/Assets/Script/BossLady/boss_lady_script.cs
+++ Shadowless/Assets/Script/BossLady/boss_lady_script.cs
@@ -2,7 +2,21 @@ using UnityEngine;
 
 public class boss_lady_script : MonoBehaviour
 {
-    
+    [SerializeField]
+    private BossState currentState = BossState.Idle;
+
+    private enum BossState
+    {
+        Idle,
+        Attack,
+        Run,
+        Walk,
+        Die,
+        Jump,
+        Fall,
+        Hit,
+        Dead
+    }
     [SerializeField]
     private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
