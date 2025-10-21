9898239ab5287e01b3eca238a05d5d3347994bc1
Oliver Bjørn Diangson Rosenkilde <olros.dev@proton.me>
2025-10-21 12:40:37 +0200

    attack and dialogue
    # Conflicts:
    #	Shadowless/Assets/Scenes/Act5.unity

--- Shadowless/Assets/Script/BossLady/boss_lady_script.cs
+++ Shadowless/Assets/Script/BossLady/boss_lady_script.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
-
+using TMPro;
+using System.Collections;
 public class boss_lady_script : MonoBehaviour
 {
     [SerializeField]
     private BossState currentState = BossState.Idle;
-
+    
+    [SerializeField]
+    private TextMeshProUGUI dialogText; // Træk din UI-tekst herind i Inspector
+    
+    [SerializeField]
+    private Animator animator;
+    
     private enum BossState
     {
         Idle,
@@ -17,12 +24,10 @@ public class boss_lady_script : MonoBehaviour
         Hit,
         Dead
     }
-    [SerializeField]
-    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(StartSequence());
     }
 
     // Update is called once per frame
@@ -31,8 +36,26 @@ public class boss_lady_script : MonoBehaviour
         
     }
 
-    void idle()
+    void Idle()
     {
         animator.SetBool("isIdle", true);
     }
+
+    IEnumerator StartSequence()
+    {
+        // Vis dialogtekst
+        dialogText.gameObject.SetActive(true);
+        dialogText.text = "You dare challenge me?";
+        
+        // Vent 3 sekunder
+        yield return new WaitForSeconds(3f);
+
+        // Skjul dialog
+        dialogText.gameObject.SetActive(false);
+
+        // Start angreb
+        currentState = BossState.Attack;
+        animator.SetBool("isAttacking", true); // Sørg for at din Animator har denne bool
+    }
+
 }
