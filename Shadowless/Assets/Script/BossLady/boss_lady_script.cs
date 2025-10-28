304a8bbc180cad06ffffcddac4a3f2f97fb47b57
Oliver Bjørn Rosenkilde <oliver.rosenkilde@gmail.com>
2025-10-28 11:14:06 +0100

    add health to boss lady

--- Shadowless/Assets/Script/BossLady/boss_lady_script.cs
+++ Shadowless/Assets/Script/BossLady/boss_lady_script.cs
@@ -1,17 +1,35 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System;
 public class boss_lady_script : MonoBehaviour
 {
-    [SerializeField]
-    private BossState currentState = BossState.Idle;
-    
-    [SerializeField]
-    private TextMeshProUGUI dialogText; // Træk din UI-tekst herind i Inspector
-    
-    [SerializeField]
-    private Animator animator;
-    
+    [SerializeField] private BossState currentState = BossState.Idle;
+
+    [SerializeField] private TextMeshProUGUI dialogText; // Træk din UI-tekst herind i Inspector
+
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float lineDelay = 3f; // tid mellem linjer
+
+
+    private String bigText =
+    "Ah, finally! The 'big' hero arrives!\n" +
+    "Took you long enough\n" +
+    "I was starting to think you got lost in my clearly labeled death traps.\n" +
+    "You must be so proud of yourself.\n" +
+    "All that jumping, dodging, and button mashing…\n" +
+    "for this.\n" +
+    "Let me guess...\n" +
+    "you’re here to ‘defeat evil’ and ‘save the kingdom’?\n" +
+    "How original.\n" +
+    "Truly, history will remember you as the nine-hundredth fool to try.\n" +
+    "But don´t worry...\n" +
+    "I´ll make your end quick.\n" + 
+    "…Okay, maybe not that quick.\n" + 
+    "I do enjoy a good scream. \n" +
+    "Now then — shall we dance, sparkle boy?";
+
     private enum BossState
     {
         Idle,
@@ -33,7 +51,7 @@ public class boss_lady_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Idle()
@@ -45,17 +63,24 @@ public class boss_lady_script : MonoBehaviour
     {
         // Vis dialogtekst
         dialogText.gameObject.SetActive(true);
-        dialogText.text = "You dare challenge me?";
-        
-        // Vent 3 sekunder
-        yield return new WaitForSeconds(3f);
-
+        yield return StartCoroutine(ShowDialogLines(bigText));
         // Skjul dialog
         dialogText.gameObject.SetActive(false);
 
         // Start angreb
         currentState = BossState.Attack;
         animator.SetBool("isAttacking", true); // Sørg for at din Animator har denne bool
+    }
+    
+    IEnumerator ShowDialogLines(string fullText)
+    {
+        string[] lines = fullText.Split('\n');
+
+        foreach (string line in lines)
+        {
+            dialogText.text = line.Trim();
+            yield return new WaitForSeconds(lineDelay);
+        }
     }
 
 }
